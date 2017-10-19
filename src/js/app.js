190100b0d559805c22caf0dda4539b0a11798ff5
Manu Hdez <manuel.jhernandez.91@me.com>
2017-10-19 19:53:15 +0100

    basic ajax request setted

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,18 @@
 $(document).ready(function(){
 
-  var url = "https://wind-bow.gomix.me/twitch-api" + "/streams/freecodecamp?callback=?"
-
+  var url = "https://wind-bow.gomix.me/twitch-api/streams/"
   var streamers = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]
+  var callback = "?callback=?"
 
-  $.getJSON(url, function(data) {
+  $.ajax({
+    type:"GET",
+    url:url + streamers[0] + callback,
+    async:false,
+    dataType:"json",
+    success: function(data) {
 
-  console.log(data);
+      console.log(data);
+    }
 
-}); //json request closing
+}); //ajax request closing
 }); //document ready closing

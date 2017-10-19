c1fd239bee377618dabb2e4534fe8e7e479398b3
Manu Hdez <manuel.jhernandez.91@me.com>
2017-10-19 19:40:40 +0100

    creating the structure for the ajax request

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
 
-  
-});
+  var url = "https://wind-bow.gomix.me/twitch-api" + "/streams/freecodecamp?callback=?"
+
+  var streamers = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]
+
+  $.getJSON(url, function(data) {
+
+  console.log(data);
+
+}); //json request closing
+}); //document ready closing
